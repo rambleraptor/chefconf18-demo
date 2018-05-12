e4394432c555004d841a63e6e389ea9179125789
Alex Stephen <alexstephen@google.com>
2018-05-12 21:18:34 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# onprem
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook:: onprem
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+python_runtime '2'
